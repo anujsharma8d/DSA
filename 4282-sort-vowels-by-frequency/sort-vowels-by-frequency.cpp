ceb1621702a3d440a3e3cdebb904887222414b5f
Anuj Sharma <anujsharma8d@gmail.com>
2026-05-08 15:46:51 +0530

    Time: 17 ms (71.45%) | Memory: 17.8 MB (84.40%) - LeetSync

---
+++ 4282-sort-vowels-by-frequency/sort-vowels-by-frequency.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    string sortVowels(string s) {
+        int n=s.size();
+        vector<char> vow;
+        for(int i=0;i<n;i++){
+            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u'){
+                vow.push_back(s[i]);
+            }
+        }
+        sort(vow.begin(),vow.end());
+        int n1=vow.size();
+        map<char,int> mp;
+        for(int i=0;i<n1;i++){
+            mp[vow[i]]++;
+        }
+        int m1=mp.size();
+        int count=0;
+        vector<pair<int,char>> v;
+        for(auto i:mp){
+            v.push_back({i.second,i.first});
+        }
+        sort(v.begin(), v.end(), [&](pair<int,char> a, pair<int,char> b){
+            if(a.first != b.first) {
+                return a.first > b.first;
+            }
+            return s.find(a.second) < s.find(b.second);
+        });
+
+        int idx=0;
+        for(auto x : v){
+            for(int i = 0; i < x.first; i++){
+                vow[idx]=x.second;
+                idx++;
+            }
+        }
+        int low=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u'){
+                s[i]=vow[low];
+                low++;
+            }
+        }
+        return s;
+    }
+};
