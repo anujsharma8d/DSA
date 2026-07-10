ece7483540ff518c5821bd57f09c5a6bef1dd5ab
Anuj Sharma <anujsharma8d@gmail.com>
2026-07-10 20:58:16 +0530

    Time: 13 ms (39.81%) | Memory: 11.5 MB (65.40%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -6,19 +6,18 @@ public:
             mpp[t[i]]++;
         }
         int left=0;
-        int right=0;
-        int count = 0;
         int minlen=INT_MAX;
-        int idx=-1;
-        while(right<s.size()){
-            if(mpp[s[right]]>0){
+        int count=0;
+        int start=0;
+        for(int right=0;right<s.size();right++){
+            mpp[s[right]]--;
+            if(mpp[s[right]]>=0){
                 count++;
             }
-            mpp[s[right]]--;
             while(count==t.size()){
-                if(right-left+1<minlen){
-                    minlen=right-left+1;
-                    idx=left;
+                if(right - left + 1 < minlen) {
+                    minlen = right - left + 1;
+                    start = left;
                 }
                 mpp[s[left]]++;
                 if(mpp[s[left]]>0){
@@ -26,13 +25,13 @@ public:
                 }
                 left++;
             }
-            right++;
+
         }
-        if(idx==-1){
+        if(minlen == INT_MAX){
             return "";
         }
         else{
-            return s.substr(idx,minlen);
+            return s.substr(start,minlen);
         }
     }
 };
