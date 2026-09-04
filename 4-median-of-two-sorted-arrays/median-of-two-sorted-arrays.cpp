72756c77963dc8fc8b400de854809cc910274772
Anuj Sharma <anujsharma8d@gmail.com>
2026-09-05 00:25:11 +0530

    Time: 1 ms (37.29%) | Memory: 95 MB (94.71%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,65 +1,43 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        if(nums1.size()>nums2.size()){
+            return findMedianSortedArrays(nums2,nums1);
+        }
+        
         int m = nums1.size();
         int n = nums2.size();
-        int pt1=0;
-        int pt2=0;
-        int count=0;
 
-        int size = m+n;
-        int idx1= (size-1)/2;
-        int idx2= size/2;
+        int l=0;
+        int r=m;
 
-        int ans1 = 0;
-        int ans2 = 0;
+        while(l<=r){
+            int Px = l+(r-l)/2;
+            int Py = (m+n+1)/2-Px;
 
-        while(pt1!=m && pt2!=n){
-            if(nums1[pt1]>nums2[pt2]){
-                if(count==idx1){
-                    ans1=nums2[pt2];
+            int x1 = (Px==0)? INT_MIN:nums1[Px-1]; 
+            int x2 = (Py==0)? INT_MIN:nums2[Py-1];
+
+            int x3 = (Px==m)? INT_MAX:nums1[Px];
+            int x4 = (Py==n)? INT_MAX:nums2[Py];
+
+            if(x1<=x4 && x2<=x3){
+                if((m+n)%2==1){
+                    return max(x1,x2);
                 }
-                if(count==idx2){
-                    ans2=nums2[pt2];
+                else{
+                    return (max(x1,x2)+min(x3,x4))/2.0;
                 }
-                pt2++;
+            }
+
+            if(x1>x4){
+                r = Px-1;
             }
             else{
-                if(count==idx1){
-                    ans1=nums1[pt1];
-                }
-                if(count==idx2){
-                    ans2=nums1[pt1];
-                }
-                pt1++;
+                l = Px+1;
             }
-                count++;
         }
-        while(pt1!=m){
-                if(count==idx1){
-                    ans1=nums1[pt1];
-                }
-                if(count==idx2){
-                    ans2=nums1[pt1];
-                }
-                pt1++;
-                count++;
-        }
-        while(pt2!=n){
-                if(count==idx1){
-                    ans1=nums2[pt2];
-                }
-                if(count==idx2){
-                    ans2=nums2[pt2];
-                }
-                pt2++;
-                count++;
-        }
-        if((m+n)%2==0){
-            return (ans1+ans2)/2.0;
-        }
-        else{
-            return (double)ans2;
-        }
+        return -1;
+        
     }
 };
