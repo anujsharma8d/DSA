c729747d91acc00fc0005b2a0837daca68d7e968
Anuj Sharma <anujsharma8d@gmail.com>
2026-09-04 23:40:13 +0530

    Time: 0 ms (100.00%) | Memory: 95.1 MB (61.10%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -3,33 +3,63 @@ public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int m = nums1.size();
         int n = nums2.size();
-        vector<int> temp;
         int pt1=0;
         int pt2=0;
+        int count=0;
+
+        int size = m+n;
+        int idx1= (size-1)/2;
+        int idx2= size/2;
+
+        int ans1 = 0;
+        int ans2 = 0;
+
         while(pt1!=m && pt2!=n){
             if(nums1[pt1]>nums2[pt2]){
-                temp.push_back(nums2[pt2]);
+                if(count==idx1){
+                    ans1=nums2[pt2];
+                }
+                if(count==idx2){
+                    ans2=nums2[pt2];
+                }
                 pt2++;
             }
             else{
-                temp.push_back(nums1[pt1]);
+                if(count==idx1){
+                    ans1=nums1[pt1];
+                }
+                if(count==idx2){
+                    ans2=nums1[pt1];
+                }
                 pt1++;
             }
+                count++;
         }
         while(pt1!=m){
-            temp.push_back(nums1[pt1]);
+                if(count==idx1){
+                    ans1=nums1[pt1];
+                }
+                if(count==idx2){
+                    ans2=nums1[pt1];
+                }
                 pt1++;
+                count++;
         }
         while(pt2!=n){
-            temp.push_back(nums2[pt2]);
+                if(count==idx1){
+                    ans1=nums2[pt2];
+                }
+                if(count==idx2){
+                    ans2=nums2[pt2];
+                }
                 pt2++;
+                count++;
         }
-        int mid = (m+n)/2;
         if((m+n)%2==0){
-            return (temp[mid-1]+temp[mid])/2.0;
+            return (ans1+ans2)/2.0;
         }
         else{
-            return (double)temp[mid];
+            return (double)ans2;
         }
     }
 };
