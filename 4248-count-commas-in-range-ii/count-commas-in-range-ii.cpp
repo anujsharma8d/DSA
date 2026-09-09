98eebd93894a8dfa5d87721afa32a1805198dd35
Anuj Sharma <anujsharma8d@gmail.com>
2026-09-09 09:36:54 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (19.20%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -1,35 +1,14 @@
-class Solution {
-public:
-    long long countCommas(long long n) {
-        long long ans = 0;
-        if(n<1000){
-            ans= 0;
+    class Solution {
+    public:
+        long long countCommas(long long n) {
+            long long comma=0;
+            long long base=1000;
+            if(n<1000) return 0;
+            else{
+                while(base<=n){
+                comma+=(n-base+1);
+                base*=1000;}
+            }
+            return comma;
         }
-        else if(n<1000000){
-            ans=n-999;
-        }
-        else if(n<1000000000){
-            ans=(n-999999)*2;
-            ans+=999000;
-        }
-        else if(n<1000000000000){
-            ans=(n-999999999)*3;
-            ans+=999000000*2;
-            ans+=999000;
-        }
-        else if(n<1000000000000000){
-            ans=(n-999999999999)*4;
-            ans+=999000000000*3;
-            ans+=999000000*2;
-            ans+=999000;
-        }
-        else if(n<1000000000000000000){
-            ans=(n-999999999999999)*5;
-            ans+=999000000000000*4;
-            ans+=999000000000*3;
-            ans+=999000000*2;
-            ans+=999000;
-        }
-        return ans;
-    }
-};
+    };
