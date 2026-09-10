287f2125bba11ac0842d8cf0aaba973b4d2f7bbf
Anuj Sharma <anujsharma8d@gmail.com>
2026-09-10 10:01:22 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (29.76%) - LeetSync

--- 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.cpp
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.cpp
@@ -11,32 +11,56 @@
  */
 class Solution {
 public:
-    int averageOfSubtree(TreeNode* root) {
-        queue<TreeNode*> q;
-        if(root == NULL)
-            return 0;
-        vector<int> ans;
-        q.push(root);
-        while(!q.empty()){
-            TreeNode* temp = q.front();
-            q.pop();
-            ans.push_back(temp->val);
-            if(temp->left!=NULL){
-                q.push(temp->left);
-            }
-            if(temp->right!=NULL){
-                q.push(temp->right);
-            }
+    pair<int,int> helper(TreeNode* root, int &count){
+        if(root==NULL){
+            return {0,0};
         }
-        int sum=0;
-        for(int i:ans){
-            sum+=i;
-        }
-        int avg=sum/ans.size();
-        if(avg==ans[0]){
-            return 1+averageOfSubtree(root->left)+averageOfSubtree(root->right);
+        auto left = helper(root->left,count);
+        auto right = helper(root->right,count);
+
+        int sum = root->val+left.first+right.first;
+        int nodes = 1+left.second+right.second;
+
+        if(sum/nodes==root->val){
+            count++;
         }
 
-        return averageOfSubtree(root->left)+averageOfSubtree(root->right);
+        return {sum,nodes};
+    }
+
+    int averageOfSubtree(TreeNode* root) {
+        int count=0;
+        helper(root,count);
+        return count;
+
+
+
+
+        // queue<TreeNode*> q;
+        // if(root == NULL)
+        //     return 0;
+        // vector<int> ans;
+        // q.push(root);
+        // while(!q.empty()){
+        //     TreeNode* temp = q.front();
+        //     q.pop();
+        //     ans.push_back(temp->val);
+        //     if(temp->left!=NULL){
+        //         q.push(temp->left);
+        //     }
+        //     if(temp->right!=NULL){
+        //         q.push(temp->right);
+        //     }
+        // }
+        // int sum=0;
+        // for(int i:ans){
+        //     sum+=i;
+        // }
+        // int avg=sum/ans.size();
+        // if(avg==ans[0]){
+        //     return 1+averageOfSubtree(root->left)+averageOfSubtree(root->right);
+        // }
+
+        // return averageOfSubtree(root->left)+averageOfSubtree(root->right);
     }
 };
