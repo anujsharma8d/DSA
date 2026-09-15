a99c33e7e63f4de115523c27c3f8f89cda1aec6a
Anuj Sharma <anujsharma8d@gmail.com>
2026-09-15 20:12:12 +0530

    Time: 53 ms (7.97%) | Memory: 188.5 MB (11.68%) - LeetSync

---
+++ 4041-climbing-stairs-ii/climbing-stairs-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int solve(int n,int i,vector<int>& costs,vector<int> &dp){
+        if(i>=n){
+            return 0;
+        }
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        int ans=INT_MAX;
+        if(i+1<=n){
+            ans = min(ans,costs[i]+1+solve(n,i+1,costs,dp));
+        }
+        if(i+2<=n){
+            ans = min(ans,costs[i+1]+4+solve(n,i+2,costs,dp));
+        }
+        if(i+3<=n){
+            ans = min(ans,costs[i+2]+9+solve(n,i+3,costs,dp));
+        }
+
+        return dp[i]=ans;
+    }
+
+    int climbStairs(int n, vector<int>& costs) {
+        vector<int> dp(n+1,-1);
+        return solve(n,0,costs,dp);
+    }
+};
