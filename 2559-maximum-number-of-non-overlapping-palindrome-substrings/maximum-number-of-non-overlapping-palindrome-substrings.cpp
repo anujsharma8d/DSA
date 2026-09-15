7b0293a5038a340419b69e37bb6d4cc7efa58529
Anuj Sharma <anujsharma8d@gmail.com>
2026-09-15 11:34:05 +0530

    Time: 921 ms (8.90%) | Memory: 218.2 MB (13.35%) - LeetSync

--- 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    bool isPalindrome(string &s1,int low,int high){
-        while(low<high){
-            if(s1[low]!=s1[high]){
-                return false;
-            }
-            low++;
-            high--;
-        }
-        return true;
-    }
+    // bool isPalindrome(string &s1,int low,int high){
+    //     while(low<high){
+    //         if(s1[low]!=s1[high]){
+    //             return false;
+    //         }
+    //         low++;
+    //         high--;
+    //     }
+    //     return true;
+    // }
 
-    int solve(string &s, int k,int i,int j,vector<vector<int>> &dp){
+    int solve(string &s, int k,int i,int j,vector<vector<int>> &dp,vector<vector<bool>> &isPalindrome){
         int n=s.size();
         if(i>=n || j>=n){
             return 0;
@@ -19,14 +19,14 @@ public:
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
-        if(isPalindrome(s,i,j)){
-            int take = 1+solve(s,k,j+1,j+k,dp);
-            int grow = solve(s,k,i,j+1,dp);
-            int slide = solve(s,k,i+1,j+1,dp);
+        if(isPalindrome[i][j]){
+            int take = 1+solve(s,k,j+1,j+k,dp,isPalindrome);
+            int grow = solve(s,k,i,j+1,dp,isPalindrome);
+            int slide = solve(s,k,i+1,j+1,dp,isPalindrome);
             return dp[i][j]=max({take,grow,slide});
         }
-        int grow = solve(s,k,i,j+1,dp);
-        int slide = solve(s,k,i+1,j+1,dp);
+        int grow = solve(s,k,i,j+1,dp,isPalindrome);
+        int slide = solve(s,k,i+1,j+1,dp,isPalindrome);
         return dp[i][j]=max(grow,slide);
 
     }
@@ -37,8 +37,23 @@ public:
         if(k==1){
             return n;
         }
+        vector<vector<bool>> isPalindrome(n,vector<bool>(n,false));
+        for(int l=1;l<=n;l++){
+            for(int i=0;i+l<=n;i++){
+                int j=i+l-1;
+                if(i==j){
+                    isPalindrome[i][j]=true;
+                }
+                else if(i+1==j){
+                    isPalindrome[i][j]=(s[i]==s[j]);
+                }
+                else{
+                    isPalindrome[i][j] = (s[i]==s[j]) && isPalindrome[i+1][j-1];
+                }
+            }
+        }
         vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-        return solve(s,k,0,k-1,dp);
+        return solve(s,k,0,k-1,dp,isPalindrome);
 
     }
 };
