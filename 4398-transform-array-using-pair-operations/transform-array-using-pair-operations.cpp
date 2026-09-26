e8821e813ba823d39bced36ca8d460552e92de95
Anuj Sharma <anujsharma8d@gmail.com>
2026-09-27 03:47:48 +0530

    Time: 0 ms (100.00%) | Memory: 193.2 MB (80.00%) - LeetSync

---
+++ 4398-transform-array-using-pair-operations/transform-array-using-pair-operations.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canTransform(vector<int>& source, vector<int>& target) {
+        int n=source.size();
+        long long sum1=0;
+        long long sum2=0;
+        for(int x:source){
+            sum1+=x;
+        }
+        for(int x:target){
+            sum2+=x;
+        }
+        return sum1==sum2;
+    }
+};
